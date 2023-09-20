3399a8150bd72d69de5fc07faaaa9a4cc76ae52a
Alex Felix <85413825+afsilva3021@users.noreply.github.com>
2023-09-20 18:19:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-components.iframe("http://200.153.243.206/portuguese/help110.htm")
+components.iframe("http://srv-sid1.ddns.net/")
